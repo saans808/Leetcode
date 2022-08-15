bc4cd32cd121d749eaad080ed19492b71d98e6fa
Saans <92422813+saans808@users.noreply.github.com>
2022-08-15 23:30:02 +0900

    Time: 113 ms (23.87%), Space: 44.9 MB (69.75%) - LeetHub

---
+++ 704-binary-search/704-binary-search.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let answer = nums.findIndex(ans => ans == target);
+    return answer;
+};
