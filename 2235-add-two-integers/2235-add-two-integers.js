a77d06caf694516b5735af80c6c0423ac6bafded
Saans <92422813+saans808@users.noreply.github.com>
2022-08-17 21:50:30 +0900

    Time: 102 ms (26.06%), Space: 41.9 MB (63.28%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.js
+++ 2235-add-two-integers/2235-add-two-integers.js
@@ -3,6 +3,7 @@
  * @param {number} num2
  * @return {number}
  */
-var sum = function(num1, num2) {
-    return num1+num2;
+var sum = function() {
+    if (arguments[0] == 0) return arguments[1]
+    return arguments[0] + arguments[1]
 };
