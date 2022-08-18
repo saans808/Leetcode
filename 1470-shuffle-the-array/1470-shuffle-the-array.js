852211be831fb52dd0b6158762f6b9e8936af605
Saans <92422813+saans808@users.noreply.github.com>
2022-08-18 15:37:25 +0900

    Time: 107 ms (46.85%), Space: 44.8 MB (15.98%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -4,12 +4,9 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-    let nums1 = nums.slice(0, n);
-    let nums2 = nums.slice(-n);
     let ans = [];
-    for (let i =0; i<nums1.length; i++) {
-        ans.push(nums1[i]);
-        ans.push(nums2[i]);
+    for (let i =0; i<n; i++) {
+        ans.push(nums[i],nums[i+n]);
     }
     return ans;
 };
