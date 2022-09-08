62f9076c418ef8a1dda50a0656fd779757e299d1
Saans <92422813+saans808@users.noreply.github.com>
2022-09-08 23:59:34 +0900

    Time: 90 ms (45.76%), Space: 41.7 MB (85.84%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -3,6 +3,14 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    s = ' ' + s.trim();
-    return s.length - s.lastIndexOf(' ') -1;
+    let len = 0;
+    let hasStarted = false;
+    for (let i = s.length -1; i>=0; i--) {
+        if (s[i] !== ' ') hasStarted = true;
+        if(hasStarted) {
+            if(s[i] === ' ') break;
+            len++;
+        }
+    }
+    return len;
 };
