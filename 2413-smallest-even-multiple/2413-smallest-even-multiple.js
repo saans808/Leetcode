54844a248dcca8bf99bdbc2866cb458d5d4c181c
Saans <92422813+saans808@users.noreply.github.com>
2022-09-22 23:06:35 +0900

    Time: 113 ms (10.68%), Space: 42 MB (49.76%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.js
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.js
@@ -3,9 +3,5 @@
  * @return {number}
  */
 var smallestEvenMultiple = function(n) {
-    if (n % 2 == 0) {
-        return n;
-    } else {
-        return n * 2;
-    }
+    return n % 2 == 0 ? n : n * 2;
 };
