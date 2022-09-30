955d61614584af4de5c6b11697b9aab08d8ac1d0
Saans <92422813+saans808@users.noreply.github.com>
2022-09-30 23:58:01 +0900

    Time: 281 ms (13.00%), Space: 46.4 MB (94.09%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -2,20 +2,23 @@
  * @param {string} s
  * @return {number}
  */
-symbols = {
-    "I":1,
-    "V":5,
-    "X":10,
-    "L":50,
-    "C":100,
-    "D":500,
-    "M":1000
-};
+
 var romanToInt = function(s) {
-    value = 0;
+    let map = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    };
+    let num = 0;
     for(let i=0; i<s.length; i++) {
-        s[i+1] && symbols[s[i]] < symbols[s[i+1]] ? value -= symbols[s[i]]: value += symbols[s[i]]
+        const curr = map[s[i]], next = map[s[i+1]];
+        if (curr < next) num -= curr;
+        else num += curr;
     }
-    return value;
+    return num;
 
 };
