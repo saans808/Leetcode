877c9a4255f1e6bbdf2aad09259936505c6fd6f8
Saans <92422813+saans808@users.noreply.github.com>
2022-10-04 23:46:37 +0900

    Time: 101 ms (28.98%), Space: 42 MB (74.75%) - LeetHub

---
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var minimumSum = function(num) {
+    let numbers =[];
+    for (let i=0; i<4; i++) {
+        numbers.push(~~num % 10)
+        num /= 10
+    }
+    const sorted = numbers.sort((a,b) => b-a)
+    return sorted[0] + sorted[1] + (10*(sorted[2]+sorted[3]))
+};
