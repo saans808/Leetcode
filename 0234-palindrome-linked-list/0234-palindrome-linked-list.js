b78a672c448c99da5a3cab501be09d1812ac2d6c
Saans <92422813+saans808@users.noreply.github.com>
2022-10-26 21:24:55 +0900

    Time: 192 ms (84.94%), Space: 77.7 MB (55.11%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let curr = head;
+    const traverse = node => {
+        if (node === null) {
+            return true;
+        }
+        const prevIsSame = traverse(node.next);
+        const currIsSame = curr.val === node.val;
+        curr = curr.next;
+        return prevIsSame && currIsSame;
+    }
+    return traverse(head);
+};
